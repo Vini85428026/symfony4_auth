865315ae02805c5b3b81bfb13597fb0c40181395
Vinicius Pereira <vinicius@localhost.localdomain>
2018-06-25 17:26:41 -0300

    creating authentication with form login

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return new Response("<html><body><h1>Admin page!</h1></body></html>");
+    }
+
+    /**
+     * @Route("/admin/dashboard", name="dashboard")
+     */
+    public function dashboard()
+    {
+        return new Response("<html><body><h1>Admin dashboard page!</h1></body></html>");
+    }
+
+    /**
+     * @Route("/admin/relatorios", name="relatorios")
+     */
+    public function relatorios()
+    {
+        return new Response("<html><body><h1>Admin relatorios page!</h1></body></html>");
+    }
+
+    /**
+     * @Route("/login", name="login")
+     * @Template("default/login.html.twig")
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils)
+    {
+        $error = $authUtils->getLastAuthenticationError();
+        $lastUsername = $authUtils->getLastUsername();
+
+        return [
+            'error' => $error,
+            'last_username' => $lastUsername
+        ];
+    }
+}
